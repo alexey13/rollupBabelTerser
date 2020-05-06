6dbc8cc6592ca92e961d8f823497c43844a63a9b
Alexey Zhavoronkov <alex-13-05@mail.ru>
2020-05-06 21:17:08 +0300

    code

--- rollup/babel.config.js
+++ rollup/babel.config.js
@@ -1,34 +1,39 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import commonjs from '@rollup/plugin-commonjs';
 import resolve  from '@rollup/plugin-node-resolve';
 
-const def = [
-  {
-    input: './src/app.js',
-    plugins: [
-      babel({
-        "babelrc": false,
-        "runtimeHelpers": true,
-        "presets": ["@babel/preset-env"],
-        "plugins": [
-            ["@babel/transform-runtime"]
-        ],
-        "exclude": "node_modules/**"
-      }),
-      terser(),
-      resolve(),
-      commonjs({
-        include: "node_modules/**"
-      })
-    ],
-    output: {
-      file: './dist/app.min.js',
-      format: 'iife',
-      name: 'Wheel'
-    }
+const def = {
+  input: './src/app.js',
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      "plugins": [
+        ["@babel/plugin-transform-runtime"]
+      ],
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              "browsers": 'defaults, not ie <= 13, not op_mini all, not edge 18, not baidu <= 100, not and_qq <= 100'
+            },
+            "useBuiltIns": "usage",
+            corejs: '3.6'
+          }
+        ]
+      ],
+      babelHelpers: 'runtime'
+    }),
+    commonjs(),
+    terser()
+  ],
+  output: {
+    file: './dist/app.min.js',
+    format: 'iife',
   }
-];
+};
 
 /*const pluginsFiles = {
   randomLetters: './src/randomLetters.js',
@@ -53,4 +58,4 @@ const rollupEach = Object.keys(pluginsFiles).map(name => {
   }
 });*/
 
-export default def.concat([]);
+export default def;
